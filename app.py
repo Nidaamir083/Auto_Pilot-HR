b1da302300c4b91accf099a90e9f04a524d8a58e
Nida Amir <142287805+Nidaamir083@users.noreply.github.com>
2025-09-27 19:10:24 +0500

    Update app.py

--- app.py
+++ app.py
@@ -3,6 +3,8 @@ from database import init_db, get_employees, add_employee, apply_leave, get_leav
 from chatbot import setup_gemini, ask_gemini
 from utils import apply_css, load_policy_texts
 from chatbot import build_vector_store
+pip install PyPDF2
+
 
 # Load policy document
 policy_chunks = load_policy_texts("Sample_HR_Policy.pdf")
